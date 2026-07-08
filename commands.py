de00789ab5dcd54043803c187c857fcb0858ebc6
injosh2026 <injosh2026@gmail.com>
2026-07-09 02:12:28 +0300

    refactor handlers to use GameSession

--- commands.py
+++ commands.py
@@ -2,14 +2,12 @@ from constants import COORD_SCALE
 from printer import print_board
 from movement import handle_click
 from game_clock import handle_wait
-from game_state import GameState
 from game_session import create_session
 from jump import handle_jump
 
 
 def process_commands(lines, start_index, board):
     session = create_session(board)
-    game_state = GameState()
 
     for line in lines[start_index:]:
         command = line.split()
@@ -25,29 +23,11 @@ def process_commands(lines, start_index, board):
             row = y // COORD_SCALE
 
             if 0 <= row < len(session.board) and 0 <= col < len(session.board[0]):
-                game_state.game_over = session.game_over
-                session.selected = handle_click(
-                    session.board,
-                    row,
-                    col,
-                    session.selected,
-                    session.pending_moves,
-                    session.game_time,
-                    game_state,
-                )
+                handle_click(session, row, col)
 
         elif command[0] == "wait":
             ms = int(command[1])
-            game_state.game_over = session.game_over
-            session.game_time = handle_wait(
-                ms,
-                session.game_time,
-                session.board,
-                session.pending_moves,
-                session.pending_jumps,
-                game_state,
-            )
-            session.game_over = game_state.game_over
+            handle_wait(session, ms)
 
         elif command[0] == "jump":
             x = int(command[1])
@@ -57,16 +37,7 @@ def process_commands(lines, start_index, board):
             row = y // COORD_SCALE
 
             if 0 <= row < len(session.board) and 0 <= col < len(session.board[0]):
-                game_state.game_over = session.game_over
-                handle_jump(
-                    session.board,
-                    row,
-                    col,
-                    session.pending_jumps,
-                    session.pending_moves,
-                    session.game_time,
-                    game_state,
-                )
+                handle_jump(session, row, col)
 
         elif command[0] == "print" and command[1] == "board":
             print_board(session.board)
