2200461f4cb1b6b362696b41bb2ca8dcb0007444
injosh2026 <injosh2026@gmail.com>
2026-07-14 03:16:43 +0300

    Refactor view layer and stabilize architecture

--- tests1/conftest.py
+++
@@ -1,7 +0,0 @@
-from helpers import (  # noqa: F401
-    make_board,
-    make_jump,
-    make_lines,
-    make_move,
-    make_valid_stdin,
-)
--- validator.py
+++
@@ -1,5 +0,0 @@
-def is_valid_token(token):
-    if token == ".":
-        return True
-
-    return len(token) == 2 and token[0] in "bw" and token[1] in "KQRBNP"
